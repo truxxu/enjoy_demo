6ee02781cd2e0f24a639fff9ed66ca537a8971a9
2624789 <2624789@users.noreply.github.com>
2019-12-01 11:25:59 -0500

    Nabvar responsive style

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
   return (
     <div className="App">
       <Container>
-        <Row className="App-Navbar justify-content-center py-4">
+        <Row className="App-Navbar justify-content-between justify-content-md-center py-4">
           <div className="col-auto">
             <img src={logo} className="App-logo" alt="logo" />
           </div>
-          <div className="col-auto d-flex align-items-center">
+          <div className="col-auto d-none d-md-flex align-items-center">
             <div className="App-Navbar-Link-List">
               <a href="#" className="App-Navbar-Link">Mujer</a>
               <a href="#" className="App-Navbar-Link">Hombre</a>
@@ -22,7 +22,7 @@ function App() {
               <a href="#" className="App-Navbar-Link">Mascotas</a>
             </div>
           </div>
-          <div className="col-auto d-flex align-items-center">
+          <div className="col-auto d-none d-md-flex align-items-center">
             <div className="App-Social-Links">
               <a href="https://instagram.com"><span className="icon-instagram"></span></a>
               <a href="https://facebook.com"><span className="icon-facebook"></span></a>
