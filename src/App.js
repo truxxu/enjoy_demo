7002fbbc9743574b85058b301edb4e09966f97d5
2624789 <2624789@users.noreply.github.com>
2019-12-01 16:21:28 -0500

    Adds description

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import categoriaMujer from './images/mujer.png';
 import categoriaHombre from './images/hombre.png';
 import categoriaNiños from './images/niños.png';
 import categoriaMascotas from './images/mascotas.png';
+import descripcion from './images/descripcion.png';
 
 function App() {
   return (
@@ -42,7 +43,10 @@ function App() {
           <div className="col-auto d-none d-md-flex align-self-center mt-5">
             <img className="App-Header-Image" src={logo} alt="logo" />
           </div>
-          <h1 className="App-Header-Title p-4">Por que la belleza<br/><span class="App-Header-Subtitle">es para todos</span></h1>
+          <h1 className="App-Header-Title p-4">
+            Por que la belleza<br/>
+            <span className="App-Header-Subtitle">es para todos</span>
+          </h1>
           <InputGroup className="App-Header-Search align-self-center mb-4 px-2">
             <FormControl
               placeholder="Buscar un servicio o un salón"
@@ -59,7 +63,7 @@ function App() {
             <p>Haz <a href="#">click</a> y agenda tu cita a domicilio o en el salón</p>
           </div>
         </Row>
-        <Row className="App-Category-List justify-content-center">
+        <Row className="App-Category-List justify-content-center mb-4">
           <div className="col-auto App-Category-List-Item">
             <img src={categoriaMujer} className="App-Category-List-Item-Image" alt="mujer" />
             <p className="mt-3">Mujer</p>
@@ -75,6 +79,23 @@ function App() {
           <div className="col-auto App-Category-List-Item">
             <img src={categoriaMascotas} className="App-Category-List-Item-Image" alt="mascotas" />
             <p className="mt-3">Mascotas</p>
+          </div>
+        </Row>
+        <Row className="App-Description align-items-center justify-content-center py-5">
+          <div className="col-auto d-none d-md-flex align-items-end">
+            <img src={descripcion} className="App-Description-Image" alt="mujer" />
+          </div>
+          <div className="col col-md-5 justify-content-center">
+            <h1 className="mb-5">Porque la belleza es para todos.</h1>
+            <div className="App-Description-Text px-3 px-md-0">
+              <p>Lorem iquis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+              <p>
+                Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu
+                feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
+                delenit augue duis dolore te feugait nulla facilisi.
+              </p>
+              <p>Tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+            </div>
           </div>
         </Row>
       </Container>
