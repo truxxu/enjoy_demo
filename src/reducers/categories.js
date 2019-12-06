c03993c61e03125ec2fe113a7d408411ff544994
Nicolas <nicolas.b.cortes@gmail.com>
2019-12-06 17:40:17 -0500

    Crear reducer

---
+++ src/reducers/categories.js
@@ -0,0 +1,35 @@
+const initialState = {
+  list: [],
+  activeItem: null
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    // case GET_BUSINESS:
+    //   return {
+    //     ...state,
+    //     business: action.payload,
+    //     isLoading: true
+    //   };
+    // case GET_CURRENT_BUSINESS:
+    //   return {
+    //     ...state,
+    //     currentBusiness: action.payload,
+    //     isLoading: false
+    //   };
+    // case DELETE_BUSINESS:
+    //   return {
+    //     ...state,
+    //     business: state.business.filter(
+    //       business => business.id !== action.payload
+    //     )
+    //   };
+    // case ADD_BUSINESS:
+    //   return {
+    //     ...state,
+    //     business: [...state.business, action.payload]
+    //   };
+    default:
+      return state;
+  }
+}
