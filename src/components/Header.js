50ff27e7976ba0f3d4979edbd2b3f61997c374ac
alexgarciapo <alexgarciapo@gmail.com>
2019-12-11 10:45:30 -0500

    EN02-06 - Imagen Header Services
    EN02-06 - Imagen Header Services segun la catagoria que seleccione desde el Home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import InputGroup from "react-bootstrap/InputGroup";
@@ -6,12 +6,23 @@ import FormControl from "react-bootstrap/FormControl";
 import SearchInput from "./SearchInput";
 import banner from "../images/banner_servicios_ninos.jpg";
 import "../styles/Header.css"; 
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getCategories, setCategory } from "../actions/categories";
 
-function Header() {
+function Header(props) {
+  const { getCategories, list, setCategory } = props;
+  
+  
+  useEffect(() => {
+    getCategories();
+  }, []);
+  
+console.log(props.activeItem);
   return (
     <React.Fragment>
         <Row className="Header-Row flex flex-column">
-            <img className="Header-Img img-fluid" src={banner} alt="logo" />
+            <img className="Header-Img img-fluid" src={props.activeItem.header_image} alt="logo" />
             <div className="Header-Search align-self-center">
               <h1 className="Header-Title">
                 Los mejores servicios
@@ -28,4 +39,21 @@ function Header() {
   );
 }
 
-export default Header;
+const mapStateToProps = state => {
+  return {
+    activeItem: state.categories.activeItem
+  };
+};
+
+const mapDispatchToProps = {
+  getCategories,
+  setCategory
+};
+
+Header.prototype = {
+  list: PropTypes.array.isRequired,
+  getCategories: PropTypes.func.isRequired,
+  setCategory: PropTypes.func.isRequired
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
