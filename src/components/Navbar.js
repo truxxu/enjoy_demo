4a4294b19473c2ba4a2ddf2efc9b70edc8569db0
alexgarciapo <alexgarciapo@gmail.com>
2019-12-11 17:42:52 -0500

    EN01-05 - Cambios en Navbar
    EN01-05 - Se cambia la logica, solo se agrega y remueve clases de Bootstrap Flex

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
@@ -11,7 +12,6 @@ import { setCategory } from "../actions/categories";
 import SearchInput from "./SearchInput";
 
 function Navbar(props){ 
-  
   const { list, setCategory } = props;
   let AppNavBar = '';
   let LinkList = '';
@@ -19,26 +19,26 @@ function Navbar(props){
   let AppHeaderSearch = '';
   let BtnAcceder = '';
   let BtnAccederMov = '';
+  let SocialLinks = '';
+  let SearchInputDivMov = '';
   
   useEffect(() => {
     window.addEventListener('scroll', handleScroll, true);
     AppNavBar = document.getElementsByClassName('App-Navbar')[0];
     LinkList = document.getElementById('Link-List-Id');
+    SocialLinks = document.getElementById('Social-Links');
     SearchInputDiv = document.getElementById('Search-Input-Id');
     AppHeaderSearch = document.getElementsByClassName('App-Header-Search')[0];
     BtnAcceder = document.getElementById('Btn-Acceder');
-    BtnAccederMov = document.getElementById('Btn-Acceder-Mov');
-
-
+    SearchInputDivMov = document.getElementById('Search-Input-Id-Mov');
+    
     var widthBrowser = window.outerWidth;
     if(widthBrowser < 768){
-      LinkList.style.display = 'none';
-      BtnAcceder.style.display = 'none';
+      SearchInputDiv.classList.add('d-none');
+      SocialLinks.classList.add('d-none');
     }else{
-      BtnAccederMov.style.display = 'none';
+      SearchInputDiv.classList.add('d-none');
     }
-
-    SearchInputDiv.style.display = 'none';
 
   }, []);
 
@@ -52,52 +52,42 @@ function Navbar(props){
     if(widthBrowser > 768){
       if (window.pageYOffset >= 450){
         setBar();
-        BtnAccederMov.style.display = 'none';
-        
       }else{
         setBarInit();
       }
-      SearchInputDiv.classList.remove('App_Navbar_Width_Full');
     }else{
       if (window.pageYOffset >= 230){
         setBar();
-        AppNavBar.classList.remove('App_Navbar_Justify_Center');
+        SearchInputDiv.classList.add('d-none');
+        SearchInputDivMov.classList.remove('d-none');
       }else{
         setBarMovilInit();
       }
     }
-    
-   }
+  }
   
-function setBar(){  
-    AppNavBar.style = "z-index:4000";
-    AppNavBar.classList.remove('App_Navbar_Blanco');
-    AppNavBar.classList.add('App_Navbar_Dorado');
-    AppNavBar.classList.add('App_Navbar_Justify_Center');
-    AppNavBar.classList.add('App_Navbar_Width_Full');
-    AppNavBar.classList.add('position-fixed');
-    LinkList.style.display = 'none';
-    SearchInputDiv.style.display = 'block';
+  function setBar(){  
+    AppNavBar.classList.add('fixed-top');
+    LinkList.classList.remove('d-md-flex');
+    SocialLinks.classList.remove('d-md-flex');
+    SocialLinks.classList.add('d-none');
+    SearchInputDiv.classList.remove('d-none');
     AppHeaderSearch.classList.remove('mb-4');
-}
+  }
 
-function setBarInit() {
-  AppNavBar.classList.add('App_Navbar_Blanco');
-  AppNavBar.classList.remove('position-fixed');
-  SearchInputDiv.style.display = 'none';
-  LinkList.style.display = 'block';
-}
+  function setBarInit() {
+    AppNavBar.classList.remove('fixed-top');
+    LinkList.classList.add('d-md-flex');
+    SocialLinks.classList.remove('d-none');
+    SocialLinks.classList.add('d-md-flex');
+    SearchInputDiv.classList.add('d-none');
+  }
 
-function setBarMovilInit() {
-  AppNavBar.classList.add('App_Navbar_Dorado');
-  AppNavBar.classList.remove('position-fixed');
-  AppNavBar.classList.remove('App_Navbar_Width_Full');
-  AppNavBar.classList.remove('App_Navbar_Justify_Center');
-  LinkList.style.display = 'none';
-  SearchInputDiv.style.display = 'none';
-  BtnAccederMov.style.display = 'block';
-  BtnAcceder.style.display = 'none';
-}  
+  function setBarMovilInit() {
+    AppNavBar.classList.remove('fixed-top');
+    SearchInputDiv.classList.add('d-none');
+    SearchInputDivMov.classList.add('d-none');
+  }  
 
   const setActiveItem = item => {
     setCategory(item);
@@ -117,24 +107,22 @@ function setBarMovilInit() {
   };  
 
   return (
-    <Row className="App-Navbar justify-content-between justify-content-md-center py-4">
-      <div className="col-auto">
+    <container>
+    <Row className="App-Navbar justify-content-between py-4 px-5">
+      <div className="col-auto align-items-center">
         <Link to="/">
           <img src={logo} className="App-logo" alt="logo" />
         </Link>
       </div>
-      <div id="Link-List-Id" className="col-auto align-items-center">
+      <div id="Link-List-Id" className="col-auto d-none d-md-flex align-items-center">
         <div className="App-Navbar-Link-List">
           {list.map(category => renderCategory(category))}
         </div>
       </div>
-      <div id="Btn-Acceder-Mov" className="col-auto align-items-center">
-        <Button className="App-Button">Acceder</Button>
-      </div>
-      <div id="Search-Input-Id" className="App_Navbar_Width_Full align-items-center">
+      <div id="Search-Input-Id" className="col-auto align-items-center">
         <SearchInput />
       </div>
-      <div className="col-auto d-none d-md-flex align-items-center">
+      <div id="Social-Links" className="col-auto d-none d-md-flex align-items-center">
         <div className="App-Social-Links">
           <a href="https://instagram.com">
             <span className="icon-instagram"></span>
@@ -147,7 +135,11 @@ function setBarMovilInit() {
       <div id="Btn-Acceder" className="col-auto align-items-center">
         <Button className="App-Button">Acceder</Button>
       </div>
+      <div id="Search-Input-Id-Mov" className="col-auto d-none align-items-center mt-3 App_Navbar_Width_Full App_Navbar_Mar_Search">
+        <SearchInput />
+      </div>
     </Row>
+    </container>
   );
 }
 
