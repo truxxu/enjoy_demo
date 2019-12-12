433306e921b2f518bb7d3548ea87edf42b88099b
Nicolas <nicolas.b.cortes@gmail.com>
2019-12-12 15:40:47 -0500

    Componente funcional

--- src/components/SalonPage.js
+++ src/components/SalonPage.js
@@ -1,20 +1,43 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Swiper from "react-id-swiper";
 
 import Navbar from "./Navbar";
 import AlliesSlider from "./AlliesSlider";
 import Footer from "./Footer";
-import banner from "../images/header1.jpg";
+import banner from "../images/banner_servicios_ninos.jpg";
+import banner1 from "../images/header3.jpg";
+import banner2 from "../images/header1.jpg";
 import "../styles/SalonPage.css";
 
 function SalonPage() {
+  const swiperParams = {
+    loop: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false
+    },
+    slidesPerView: 1,
+    width: 375,
+    breakpoints: {
+      768: {
+        slidesPerView: 1,
+        width: 910
+      }
+    }
+  };
   return (
     <Container fluid className="Salon-Page">
       <Navbar />
-      <Container className="SalonPage-Container">
+      <Container className="SalonPage-Container px-0">
         <Row className="SalonPage-Header-Row ">
-          <img className="SalonPage-Img img-fluid" src={banner} alt="logo" />
+          <Swiper {...swiperParams} className="SalonPage-Swiper">
+            <img className="SalonPage-Img" src={banner} alt="logo" />
+            <img className="SalonPage-Img" src={banner1} alt="logo" />
+            <img className="SalonPage-Img" src={banner2} alt="logo" />
+          </Swiper>
+
           <div className="SalonPage-Header pl-4 pl-md-5 d-flex flex-column justify-content-center">
             <p className="Open-Close px-1 align-self-baseline">
               ¡ABIERTO AHORA!
