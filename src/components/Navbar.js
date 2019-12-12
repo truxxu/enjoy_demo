88119aa849b248af9eb331aa975584e92f471a69
2624789 <2624789@users.noreply.github.com>
2019-12-12 07:27:29 -0500

    Revert "Merge pull request #31 from 2624789/EN01-05"
    This reverts commit b72ccf04f1b506088c417e8834fe19dc2982dfaf, reversing
    changes made to fde813cd14ee2f06ae698e868d7ad3242bd30664.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
@@ -9,90 +8,14 @@ import PropTypes from "prop-types";
 import logo from "../images/logo.png";
 import "../styles/Navbar.css";
 import { setCategory } from "../actions/categories";
-import SearchInput from "./SearchInput";
 
-function Navbar(props){ 
+function Navbar(props) {
   const { list, setCategory } = props;
-  let AppNavBar = '';
-  let LinkList = '';
-  let SearchInputDiv = '';
-  let AppHeaderSearch = '';
-  let BtnAcceder = '';
-  let BtnAccederMov = '';
-  let SocialLinks = '';
-  let SearchInputDivMov = '';
-  
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll, true);
-    AppNavBar = document.getElementsByClassName('App-Navbar')[0];
-    LinkList = document.getElementById('Link-List-Id');
-    SocialLinks = document.getElementById('Social-Links');
-    SearchInputDiv = document.getElementById('Search-Input-Id');
-    AppHeaderSearch = document.getElementsByClassName('App-Header-Search')[0];
-    BtnAcceder = document.getElementById('Btn-Acceder');
-    SearchInputDivMov = document.getElementById('Search-Input-Id-Mov');
-    
-    var widthBrowser = window.outerWidth;
-    if(widthBrowser < 768){
-      SearchInputDiv.classList.add('d-none');
-      SocialLinks.classList.add('d-none');
-    }else{
-      SearchInputDiv.classList.add('d-none');
-    }
-
-  }, []);
-
-  const handleScroll = () => {
-    positionBar();
-  };
-
-  function positionBar() {
-    var widthBrowser = window.outerWidth;
-  
-    if(widthBrowser > 768){
-      if (window.pageYOffset >= 450){
-        setBar();
-      }else{
-        setBarInit();
-      }
-    }else{
-      if (window.pageYOffset >= 230){
-        setBar();
-        SearchInputDiv.classList.add('d-none');
-        SearchInputDivMov.classList.remove('d-none');
-      }else{
-        setBarMovilInit();
-      }
-    }
-  }
-  
-  function setBar(){  
-    AppNavBar.classList.add('fixed-top');
-    LinkList.classList.remove('d-md-flex');
-    SocialLinks.classList.remove('d-md-flex');
-    SocialLinks.classList.add('d-none');
-    SearchInputDiv.classList.remove('d-none');
-    AppHeaderSearch.classList.remove('mb-4');
-  }
-
-  function setBarInit() {
-    AppNavBar.classList.remove('fixed-top');
-    LinkList.classList.add('d-md-flex');
-    SocialLinks.classList.remove('d-none');
-    SocialLinks.classList.add('d-md-flex');
-    SearchInputDiv.classList.add('d-none');
-  }
-
-  function setBarMovilInit() {
-    AppNavBar.classList.remove('fixed-top');
-    SearchInputDiv.classList.add('d-none');
-    SearchInputDivMov.classList.add('d-none');
-  }  
 
   const setActiveItem = item => {
     setCategory(item);
   };
-  
+
   const renderCategory = category => {
     return (
       <Link
@@ -104,25 +27,21 @@ function Navbar(props){
         {category.name}
       </Link>
     );
-  };  
+  };
 
   return (
-    <container>
-    <Row className="App-Navbar justify-content-between py-4 px-5">
-      <div className="col-auto align-items-center">
+    <Row className="App-Navbar justify-content-between justify-content-md-center py-4">
+      <div className="col-auto">
         <Link to="/">
           <img src={logo} className="App-logo" alt="logo" />
         </Link>
       </div>
-      <div id="Link-List-Id" className="col-auto d-none d-md-flex align-items-center">
+      <div className="col-auto d-none d-md-flex align-items-center">
         <div className="App-Navbar-Link-List">
           {list.map(category => renderCategory(category))}
         </div>
       </div>
-      <div id="Search-Input-Id" className="col-auto align-items-center">
-        <SearchInput />
-      </div>
-      <div id="Social-Links" className="col-auto d-none d-md-flex align-items-center">
+      <div className="col-auto d-none d-md-flex align-items-center">
         <div className="App-Social-Links">
           <a href="https://instagram.com">
             <span className="icon-instagram"></span>
@@ -132,14 +51,10 @@ function Navbar(props){
           </a>
         </div>
       </div>
-      <div id="Btn-Acceder" className="col-auto align-items-center">
+      <div className="col-auto d-flex align-items-center">
         <Button className="App-Button">Acceder</Button>
       </div>
-      <div id="Search-Input-Id-Mov" className="col-auto d-none align-items-center mt-3 App_Navbar_Width_Full App_Navbar_Mar_Search">
-        <SearchInput />
-      </div>
     </Row>
-    </container>
   );
 }
 
