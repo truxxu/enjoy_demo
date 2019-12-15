f7d19afc5c9340bc5a12f96e4252f38097943a45
alexgarciapo <alexgarciapo@gmail.com>
2019-12-15 16:18:46 -0500

    EN04-01 - Reducer Autenticación
    EN04-01 - Reducer Autenticación

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
 
 import categories from "./categories";
+import authentication from "./authentication";
 
 export default combineReducers({
-  categories
+  categories,
+  authentication
 });
+
