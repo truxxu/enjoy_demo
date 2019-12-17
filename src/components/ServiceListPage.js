44456d9ba0e4c2e59e8f0b7419bb0ed2583cefb1
Nicolas <nicolas.b.cortes@gmail.com>
2019-12-17 14:56:36 -0500

    Componente Header funcional

--- src/components/ServiceListPage.js
+++ src/components/ServiceListPage.js
@@ -12,28 +12,51 @@ import Filters from "./Filters";
 import { getServices } from "../actions/services";
 
 function ServiceListPage(props) {
-  const { getServices, list } = props;
-
+  const { getServices, list, activeItem } = props;
   useEffect(() => {
     getServices();
   }, []);
 
-  const renderService = (service) => {
-    return(
-      <ServiceListItem key= {service.id} data={service} />
-    )
+  const renderService = service => {
+    return <ServiceListItem key={service.id} data={service} />;
   };
-  
+  if (activeItem) {
+    if (activeItem.name === "Mujer") {
+      var dataHeader = {
+        image: activeItem.header_image,
+        title: "los mejores cervicios",
+        subTitle: "para las Mujeres"
+      };
+    }
+    if (activeItem.name === "Hombre") {
+      var dataHeader = {
+        image: activeItem.header_image,
+        title: "los mejores cervicios",
+        subTitle: "para las hombres"
+      };
+    }
+    if (activeItem.name === "Niños") {
+      var dataHeader = {
+        image: activeItem.header_image,
+        title: "los mejores cervicios",
+        subTitle: "para las niños"
+      };
+    }
+    if (activeItem.name === "mascotas") {
+      var dataHeader = {
+        image: activeItem.header_image,
+        title: "los mejores cervicios",
+        subTitle: "para las mascotas"
+      };
+    }
+  }
+
   return (
     <Container fluid className="Service-List">
       <Navbar />
-      <Header />
+      <Header data={dataHeader} />
       <Filters />
-      <Container>
-        {
-          list.map(service => renderService(service))
-        }
-      </Container>
+      <Container>{list.map(service => renderService(service))}</Container>
       <Footer />
     </Container>
   );
@@ -41,18 +64,19 @@ function ServiceListPage(props) {
 
 const mapStateToProps = state => {
   return {
-    list: state.services.list
+    list: state.services.list,
+    activeItem: state.categories.activeItem
   };
 };
 
 const mapDispatchToProps = {
-  getServices,
+  getServices
 };
 
 ServiceListPage.prototype = {
   list: PropTypes.array.isRequired,
-  getServices: PropTypes.func.isRequired,
+  activeItem: PropTypes.object.isRequired,
+  getServices: PropTypes.func.isRequired
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ServiceListPage);
-
