c7eeb760e1d1ecac655d04d70f8a757444295c24
Daniel Trujillo <ganimedes@firemail.cc>
2020-01-17 16:50:31 -0500

    created modal for nonusers

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -12,124 +12,157 @@ import { showForm } from "../actions/bookings";
 const BookingForm = (props) => {
 
   const { show, showForm } = props;
+  let token = localStorage.getItem('token');
 
-  return (
-    <Modal show={show} onHide={() => showForm(false)}>
-      <div className="Booking-Form">
-        <div className="Booking-Form-Header">
-          <div className="Title-Box d-flex flex-row justify-content-between">
-            <p>Nombre del Salón</p>
-            <Button
-              style={{padding: 0}}
-              variant={'none'}
-              onClick={ () => showForm(false)}>
-              <p>X</p>
-            </Button>
-          </div>
-          <p>Tiempo estimado de tu reserva: X:XX horas</p>
-        </div>
-        <div className="Booking-Form-Body">
-          <p>Servicios</p>
-          <div className="Services-Box d-flex flex-row flex-wrap">
-            <div
-              className="Service d-flex flex-row justify-content-around
-              align-items-center">
-              <p>Servicio seleccionado 1</p>
-              <Button variant={'none'}>
+  console.log(token);
+
+  if (token !== null) {
+    return (
+      <Modal show={show} onHide={() => showForm(false)}>
+        <div className="Booking-Form">
+          <div className="Booking-Form-Header">
+            <div className="Title-Box d-flex flex-row justify-content-between">
+              <p>Nombre del Salón</p>
+              <Button
+                style={{padding: 0}}
+                variant={'none'}
+                onClick={ () => showForm(false)}>
                 <p>X</p>
               </Button>
             </div>
-            <div
-              className="Service d-flex flex-row justify-content-around
-              align-items-center">
-              <p>Servicio seleccionado 2</p>
-              <Button variant={'none'}>
-                <p>X</p>
-              </Button>
+            <p>Tiempo estimado de tu reserva: X:XX horas</p>
+          </div>
+          <div className="Booking-Form-Body">
+            <p>Servicios</p>
+            <div className="Services-Box d-flex flex-row flex-wrap">
+              <div
+                className="Service d-flex flex-row justify-content-around
+                align-items-center">
+                <p>Servicio seleccionado 1</p>
+                <Button variant={'none'}>
+                  <p>X</p>
+                </Button>
+              </div>
+              <div
+                className="Service d-flex flex-row justify-content-around
+                align-items-center">
+                <p>Servicio seleccionado 2</p>
+                <Button variant={'none'}>
+                  <p>X</p>
+                </Button>
+              </div>
+              <div
+                className="Service d-flex flex-row justify-content-around
+                align-items-center">
+                <p>Servicio seleccionado 3</p>
+                <Button variant={'none'}>
+                  <p>X</p>
+                </Button>
+              </div>
             </div>
-            <div
-              className="Service d-flex flex-row justify-content-around
-              align-items-center">
-              <p>Servicio seleccionado 3</p>
-              <Button variant={'none'}>
-                <p>X</p>
-              </Button>
+            <div className="Date-Box">
+              <p>Selecciona Fecha y Hora</p>
+              <p>**Calendario**</p>
             </div>
-          </div>
-          <div className="Date-Box">
-            <p>Selecciona Fecha y Hora</p>
-            <p>**Calendario**</p>
-          </div>
-          <div className="Time-Box">
-          </div>
-          <div className="Input-Box">
-            <p>¿Prefieres algún profesional?</p>
-            <InputGroup className="mb-3">
-              <FormControl
-                maxLength={40}
-                className="Modal-Input"
-                aria-label="pro_name"
-                aria-describedby="basic-addon1"
-                // onChange={}
-              />
-            </InputGroup>
-          </div>
-          <div className="Payment-Box">
-            <p>Selecciona una forma de pago</p>
-            <div className="Cash d-flex flex-row">
-              <InputGroup.Checkbox
-                aria-label="cash"
-                className="checkbox"
-                // checked={false}
-                // onChange={}
-              />
-              <p>Pagar en el centro de belleza</p>
+            <div className="Time-Box">
             </div>
-            <div className="Online d-flex flex-row">
-              <InputGroup.Checkbox
-                aria-label="online"
-                className="checkbox"
-                // checked={false}
-                // onChange={}
-              />
-              <p>¡Paga online!</p>
-            </div>
-          </div>
-          <div
-            className="Coupon-Box d-flex flex-row justify-content-between
-            align-content-end">
-              <InputGroup>
+            <div className="Input-Box">
+              <p>¿Prefieres algún profesional?</p>
+              <InputGroup className="mb-3">
                 <FormControl
                   maxLength={40}
-                  placeholder={'Ingresa tu cupón de descuento'}
                   className="Modal-Input"
-                  aria-label="coupon"
+                  aria-label="pro_name"
                   aria-describedby="basic-addon1"
                   // onChange={}
                 />
               </InputGroup>
-            <div className="Total-Price">
-              <p>Precio total</p>
-              <p className="Total">$9.999.999</p>
+            </div>
+            <div className="Payment-Box">
+              <p>Selecciona una forma de pago</p>
+              <div className="Cash d-flex flex-row">
+                <InputGroup.Checkbox
+                  aria-label="cash"
+                  className="checkbox"
+                  // checked={false}
+                  // onChange={}
+                />
+                <p>Pagar en el centro de belleza</p>
+              </div>
+              <div className="Online d-flex flex-row">
+                <InputGroup.Checkbox
+                  aria-label="online"
+                  className="checkbox"
+                  // checked={false}
+                  // onChange={}
+                />
+                <p>¡Paga online!</p>
+              </div>
+            </div>
+            <div
+              className="Coupon-Box d-flex flex-row justify-content-between
+              align-content-end">
+                <InputGroup>
+                  <FormControl
+                    maxLength={40}
+                    placeholder={'Ingresa tu cupón de descuento'}
+                    className="Modal-Input"
+                    aria-label="coupon"
+                    aria-describedby="basic-addon1"
+                    // onChange={}
+                  />
+                </InputGroup>
+              <div className="Total-Price">
+                <p>Precio total</p>
+                <p className="Total">$9.999.999</p>
+              </div>
+            </div>
+            <div className="Booking-Form-Footer">
+              <div className="d-flex justify-content-center">
+                <Button
+                  // onClick={handleCloseAlert}
+                  className="justify-content-center Modal-Button-Active
+                  Register-Modal-Button"
+                >
+                  ¡RESERVAR YA!
+                </Button>
+              </div>
+              <p>Consulta nuestras <a href="#">políticas de pago</a> y
+              <a href="#"> cancelación de servicio</a></p>
             </div>
           </div>
-          <div className="Booking-Form-Footer">
-            <div className="d-flex justify-content-center">
+        </div>
+      </Modal>
+    )
+  } else {
+    return (
+      <Modal centered show={show} onHide={() => showForm(false)}>
+        <div className="Non-User-Modal">
+          <div className="Non-User-Modal-Header">
+            Por favor registrate o inicia sesión <br /> para poder reservar tu servicio
+          </div>
+          <div className="Non-User-Modal-Footer">
+            <div className="d-flex justify-content-around">
               <Button
                 // onClick={handleCloseAlert}
                 className="justify-content-center Modal-Button-Active
                 Register-Modal-Button"
               >
-                ¡RESERVAR YA!
+                REGISTRARME
+              </Button>
+              <Button
+                // onClick={handleCloseAlert}
+                className="justify-content-center Modal-Button-Active
+                Register-Modal-Button"
+              >
+                INICIAR SESIÓN
               </Button>
             </div>
-            <p>Consulta nuestras <a href="#">políticas de pago</a> y
-            <a href="#"> cancelación de servicio</a></p>
           </div>
         </div>
-      </div>
-    </Modal>
-  )
+      </Modal>
+    )
+  }
 }
 
 const mapStateToProps = state => {
