9d2f1be2b83161b9e46d9eafce41eeb8b2edc155
nibcortes04 <44903595+nibcortes04@users.noreply.github.com>
2020-02-07 16:03:36 -0500

    Register funcional (#174)
    * Register funcional
    
    * Mensaje de registro funcional

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -9,137 +9,136 @@ import { UPDATE_USER_FIELD } from ".";
 export const authUser = (username, password) => dispatch => {
   axios
     .post(env.apiUrl + "auth/login/", {
-        username: username,
-        password: password
+      username: username,
+      password: password
     })
     .then(res => {
       dispatch({
         type: LOGIN,
         payload: res.data
       });
-      localStorage.setItem('token',res.data.token);
+      localStorage.setItem("token", res.data.token);
     })
-    .catch(err =>{
-      let errorMessage = '';
-      if(err.response) {
+    .catch(err => {
+      let errorMessage = "";
+      if (err.response) {
         errorMessage = `\nDatos incorrectos.`;
       }
-      window.alert("No se pudo completar el registro. " + errorMessage);
+      window.alert("No se pudo iniciar sesión. " + errorMessage);
     });
 };
 
 export const validateUser = () => dispatch => {
-  let token = localStorage.getItem('token');
-  if(token!=null){
+  let token = localStorage.getItem("token");
+  if (token != null) {
     const headers = {
-      'Content-Type': 'application/json',
-      'Authorization': 'Token ' + token
+      "Content-Type": "application/json",
+      Authorization: "Token " + token
     };
     axios
-    .get(env.apiUrl + "auth/user/", {
-      headers: headers
-    })
-    .then(res => {
-      dispatch({
-        type: USER,
-        payload: { user: res.data, token: token}
+      .get(env.apiUrl + "auth/user/", {
+        headers: headers
+      })
+      .then(res => {
+        dispatch({
+          type: USER,
+          payload: { user: res.data, token: token }
+        });
+      })
+      .catch(err => {
+        localStorage.removeItem("token");
+        dispatch({
+          type: USER_LOGOUT
+        });
+        window.alert("No se pudo realizar la validacion del usuario.");
       });
-    })
-    .catch(err =>{
-      localStorage.removeItem('token');
-      dispatch({
-        type: USER_LOGOUT
-      });
-      window.alert("No se pudo realizar la validacion del usuario.");
-    });
   }
 };
 
 export const logOut = () => dispatch => {
-  let token = localStorage.getItem('token');
-  if(token!=null){
+  let token = localStorage.getItem("token");
+  if (token != null) {
     const headers = {
-      'Content-Type': 'application/json',
-      'Authorization': 'Token ' + token
+      "Content-Type": "application/json",
+      Authorization: "Token " + token
     };
     axios
-    .post(env.apiUrl + "auth/logout/", null, {
-      headers: headers
-    })
-    .then(res => {
-      localStorage.removeItem('token');
-      dispatch({
-        type: USER_LOGOUT,
-        payload: res.data
+      .post(env.apiUrl + "auth/logout/", null, {
+        headers: headers
+      })
+      .then(res => {
+        localStorage.removeItem("token");
+        dispatch({
+          type: USER_LOGOUT,
+          payload: res.data
+        });
+      })
+      .catch(err => {
+        window.alert("No se pudo realizar el cierre de sesión.");
       });
-    })
-    .catch(err =>{
-      window.alert("No se pudo realizar el cierre de sesión.");
-    });
   }
 };
 
-export const updateUserField = (data) => dispatch => {
-    dispatch({
-      type: UPDATE_USER_FIELD,
-      payload: data
-    });
-
+export const updateUserField = data => dispatch => {
+  dispatch({
+    type: UPDATE_USER_FIELD,
+    payload: data
+  });
 };
 
-export const updateUser = (data) => dispatch => {
-  let token = localStorage.getItem('token');
-  if(token!=null){
+export const updateUser = data => dispatch => {
+  let token = localStorage.getItem("token");
+  if (token != null) {
     const headers = {
-      'Content-Type': 'application/json',
-      'Authorization': 'Token ' + token
+      "Content-Type": "application/json",
+      Authorization: "Token " + token
     };
     axios
-    .put(env.apiUrl + "auth/user/", data, {
-      headers: headers
-    })
-    .then(res => {
-      dispatch({
-        type: USER,
-        payload: { user: res.data, token: token}
+      .put(env.apiUrl + "auth/user/", data, {
+        headers: headers
+      })
+      .then(res => {
+        dispatch({
+          type: USER,
+          payload: { user: res.data, token: token }
+        });
+        validateUser();
+      })
+      .catch(err => {
+        window.alert("No se pudo actualizar el usuario.");
       });
-      validateUser();
-    })
-    .catch(err =>{
-      window.alert("No se pudo actualizar el usuario.");
-    });
   }
 };
 
-export const registerUser = (data) => dispatch => {
+export const registerUser = data => dispatch => {
   axios
     .post(env.apiUrl + "auth/register/", {
-        first_name: data.first_name,
-        last_name: data.last_name,
-        phone: data.phone,
-        username: data.username,
-        birth_date: data.birth_date,
-        password: data.password,
+      first_name: data.first_name,
+      last_name: data.last_name,
+      phone: data.phone,
+      username: data.username,
+      birth_date: data.birth_date,
+      password: data.password
     })
     .then(res => {
       dispatch({
         type: LOGIN,
         payload: res.data
       });
-      localStorage.setItem('token', res.data.token);
+      localStorage.setItem("token", res.data.token);
     })
-    .catch(err =>{
-      let errorMessage = '';
-      if(err.response) {
-        let errors = err.response.data
+    .catch(err => {
+      let errorMessage = "";
+      if (err.response) {
+        let errors = err.response.data;
         const ATTRIBUTES = {
-          first_name: 'Nombres',
-          last_name: 'Apellidos',
-          phone: 'Celular',
-          username: 'Correo',
-          birth_date: 'Fecha de nacimiento',
-          password: 'Constraseña',
-        }
+          first_name: "Nombres",
+          last_name: "Apellidos",
+          phone: "Celular",
+          username: "Correo",
+          birth_date: "Fecha de nacimiento",
+          password: "Constraseña"
+        };
         for (let key in errors) {
           errorMessage += `\n${ATTRIBUTES[key]}: ${errors[key]}`;
         }
@@ -150,21 +149,20 @@ export const registerUser = (data) => dispatch => {
 
 export const socialAuth = (username, tokenId, socialRed) => dispatch => {
   axios
-    .post(env.apiUrl + 'auth/social/'+socialRed+'/', {
-        access_token: tokenId,
-        username: username
+    .post(env.apiUrl + "auth/social/" + socialRed + "/", {
+      access_token: tokenId,
+      username: username
     })
     .then(res => {
       dispatch({
         type: LOGIN,
         payload: res.data
       });
-      if(res.data.token){
-        localStorage.setItem('token',res.data.token);
+      if (res.data.token) {
+        localStorage.setItem("token", res.data.token);
       }
     })
-    .catch(err =>{
+    .catch(err => {
       window.alert("No se pudo iniciar sesión con " + socialRed);
     });
 };
-
